721f02ac1d1c6c4fd3c3e80949f836123818b162
Luis Justin <luisjustin72@gmail.com>
2022-10-03 06:13:58 -0300

    Parte do meu playground

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from validators.CpfValidator import CpfValidator
+from validators.CnpjValidator import CnpjValidator
+def run():
+    # cpf = '01386292001'
+    # vCPF = CpfValidator(cpf)
+    # print(vCPF.is_valid())
 
-def run(name):
-    pass
+    cnpj = '47.661.114/0001-46'
+    vCNPJ = CnpjValidator(cnpj)
+    print(vCNPJ)
+
 
 if __name__ == '__main__':
     run()
